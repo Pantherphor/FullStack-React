c1ba48aad6002dfa54cd4c95fac695dd61f3b38c
doobzin <34076447+doobzin@users.noreply.github.com>
2021-02-20 22:42:40 +0200

    Project structure refactor

--- index.js
+++ index.js
@@ -1,28 +1,10 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+_ = require('./services/passport');
 
 
 const PORT = process.env.PORT || 5000;
 const app = express(); //ensure to use one express app
- 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSercrit,
-    callbackURL: '/auth/google/callback'
-},
-(accessToken, refreshToken, profile, done)=> {
-    console.log('access token', accessToken);
-    console.log('refresh token', refreshToken);
-    console.log('profile', profile);
-}));
 
-//request handlers
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}));
-
-app.get('/auth/google/callback', passport.authenticate('google'));
+require('./routes/authRoutes')(app);
 
 app.listen(PORT);
