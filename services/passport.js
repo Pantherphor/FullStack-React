b62417659504a08be47c97a671ab49bd685a22e4
doobzin <34076447+doobzin@users.noreply.github.com>
2021-02-21 14:10:34 +0200

    adding mongodb user handling logic

--- services/passport.js
+++ services/passport.js
@@ -5,14 +5,40 @@ const keys = require('../config/keys');
 const mongoose = require('mongoose');
 const User = mongoose.model('users');
 
+//serialize user
+passport.serializeUser((user, done) => {
+    done(null, user.id); //the error will most likely not happen here, hence the null
+});
+
+//deserialize the user
+passport.deserializeUser((id, done) =>{
+    User.findById(id)
+    .then(user => {
+        done(null, user);
+    });
+});
+
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSercrit,
     callbackURL: '/auth/google/callback'
 },
 (accessToken, refreshToken, profile, done)=> {
-    new User({ googleID: profile.id}).save();
+
+    //find out if user exist within our mangoDb collection: users
+    User.findOne(({ googleID: profile.id}))
+    .then((existingUser) => {
+        if(existingUser){
+            done(null, existingUser);
+        }else{
+            new User({ googleID: profile.id})
+            .save()
+            .then(user => done(null, user));
+        }
+    });
+
     console.log('access token', accessToken);
     console.log('refresh token', refreshToken);
     console.log('profile', profile);
+
 }));
