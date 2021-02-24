8949d15d5190e26205bdd4a4ed60d57f51827c61
doobzin <34076447+doobzin@users.noreply.github.com>
2021-02-24 06:40:13 +0200

    adding header logo logic to decide the next route, survey or landing

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
 
@@ -24,9 +25,11 @@ class Header extends Component {
         return (
             <nav>
                 <div className="nav-wrapper">
-                    <a className= "left brand-log">
+                    <Link 
+                    to={ this.props.auth ? '/surveys': '/' } 
+                    className= "left brand-log">
                         Full-Stack-React
-                    </a>
+                    </Link>
                     <ul className= "right">
                         { this.renderConstent() }
                     </ul>
