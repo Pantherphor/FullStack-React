b424fbc2ad129ee37a525000e90a621d87f2b8ec
doobzin <34076447+doobzin@users.noreply.github.com>
2021-02-24 06:28:54 +0200

    adding landing page styles and logic amend

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import react from 'react';
+
+const Landing = () => {
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h1>
+                Full-Stack-React
+            </h1>
+            Collect feedback from you users
+        </div>
+    )
+}
+
+export default  Landing;
